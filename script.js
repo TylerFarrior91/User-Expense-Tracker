96fd36b1f319874eb4a9681abf85d4793d071553
Tyler Farrior <tylerfarrior@gmail.com>
2024-01-09 19:03:14 -0500

    adding code to js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const balanceElement = document.getElementById('balance');
 const incomeElement = document.getElementById('income');
-const expenseElement = document.getElementById('expense');
+const expenseElement = document.getElementById('expense');const historyListElement = document.getElementById('historyList');
+const textElement = document.getElementById('text');
+const amountElement = document.getElementById('amount');
