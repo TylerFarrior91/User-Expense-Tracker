7e3f2195f7c3f04f05903aaae210434db239a9a0
Tyler Farrior <tylerfarrior@gmail.com>
2024-01-13 19:10:14 -0500

    added function to js file

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const incomeElement = document.getElementById('income');
 const expenseElement = document.getElementById('expense');const historyListElement = document.getElementById('historyList');
 const textElement = document.getElementById('text');
 const amountElement = document.getElementById('amount');
+
+let balance = 0;
+let income = 0;
+let expense = 0;
+const transactions = [];
+
+function updateBalance() {
+    balance = income - expense;
+    balanceElement.innerText = `$${balance.toFixed(2)}`;
+    incomeElement.innerText = `$${income.toFixed(2)}`;
+    expenseElement.innerText = `$${expense.toFixed(2)}`;
+}
